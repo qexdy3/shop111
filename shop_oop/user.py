0d6c582a9cb1cb8b74e83557f1b60f99bfbb7a6e
qexdy3 <154340027+qexdy3@users.noreply.github.com>
2026-02-27 17:38:47 +0600

    Integrate Cart functionality into User class

--- shop_oop/user.py
+++ shop_oop/user.py
@@ -1,12 +1,25 @@
+from cart import Cart
+
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.history = []
+        self.cart = Cart()
 
     def buy(self, product):
-        self.history.append(product.name)
-        product.sold += 1
+        self.cart.add(product)
+        product.sold += 1 
+
+    def checkout(self):
+        if not self.cart.items:
+            return False, 0
+        total = self.cart.total()
+        for item, qty in self.cart.to_list():
+            for _ in range(qty):
+                self.history.append(item.name)
+        self.cart.clear()
+        return True, total
 
     def change_password(self, new_pass):
         self.password = new_pass
